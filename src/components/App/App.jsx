9d209ca56a4877274a22081886a898d0b0bab4f7
Pedro Plasencia <pjpv9011@gmail.com>
2022-02-18 00:16:47 +0100

    feat: background and main grid

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,45 +1,29 @@
-import logo from "../../assets/images/Logo-PEE.png";
+import back1 from "../../assets/images/main-back-1.jpg";
+import back2 from "../../assets/images/main-back-2.jpg";
+import back3 from "../../assets/images/main-back-3.jpg";
+import { getRandomValue } from "../../utils/arrayHelpers";
+import { Col, Container, NextUIProvider, Row } from "@nextui-org/react";
 import "./App.css";
 import Store from "../../store/Store";
-import Data from "../Data/Data";
 
 function App() {
   return (
     <Store>
-      <main className="App">
-        <section className="pee-section App-section">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p className="pee-main-title">
-            Aplicación base usando Vite + React con control de state sin Redux!
-          </p>
-          <p className="pee-subtitle">
-            Desarrollado en vivo en el canal de{" "}
-            <a href="https://my.bio/prog-es">Programación en español</a>
-          </p>
-        </section>
-        <section className="react-section App-section">
-          <Data></Data>
-          <p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              React
-            </a>
-            {" | "}
-            <a
-              className="App-link"
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Vite
-            </a>
-          </p>
-        </section>
-      </main>
+      <NextUIProvider>
+        <main
+          className="App"
+          style={{
+            backgroundImage: `url(${getRandomValue([back1, back2, back3])})`,
+          }}
+        >
+          <Container fluid responsive gap={0}>
+            <Row className="row-container" justify="center">
+              {/* TODO: main login/register floating card */}
+              <Col className="col-container"></Col>
+            </Row>
+          </Container>
+        </main>
+      </NextUIProvider>
     </Store>
   );
 }
---
+++ src/utils/arrayHelpers.js
@@ -0,0 +1,3 @@
+export const getRandomValue = (array) => {
+  return array[Math.floor(Math.random() * array.length)];
+};
