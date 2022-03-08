54121e34f6aafacbcb747aff9841c4a7c2176377
Pedro Plasencia <pjpv9011@gmail.com>
2022-03-09 00:06:10 +0100

    feat: buttons with animations

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -4,10 +4,25 @@ import pokeball from "../../assets/images/pokeball.png";
 //TODO: fetch pokemon detail
 const ListItem = ({ value }) => {
   const { url, name } = value;
+
+  const selectPokemon = (pokemonUrl, name) => {
+    const buttonElem = document.getElementById(name);
+    buttonElem.classList.add("animated-btn");
+    setTimeout(() => {
+      buttonElem.classList.remove("animated-btn");
+    }, 1000);
+  };
+
   return (
-    <li className="list-item">
-      <img src={pokeball} className="pokeball-icon" />
-      <a href={url}>{name}</a>
+    <li>
+      <button
+        id={name}
+        className="list-item"
+        onClick={() => selectPokemon(url, name)}
+      >
+        <img src={pokeball} className="pokeball-icon" />
+        <span className="pokemon-btn">{name}</span>
+      </button>
     </li>
   );
 };
